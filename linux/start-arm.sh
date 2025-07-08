f101f217f798101b7364337ff4e37422cf8f0303
Niam3231 <niam3231@hotmail.com>
2025-07-08 23:10:20 +0200

    Fixed bugs
    Made the lines better to read on suggestion of TheAutisticSlavicBoy (https://www.reddit.com/user/TheAutisticSlavicBoy/)
    
    Fixed bug to not get back to other questions after stopping with donating, suggestion from
    astraleureka.
    
    Co-Authored-By: astraleureka <121209682+astraleureka@users.noreply.github.com>

--- linux/start-arm.sh
+++ linux/start-arm.sh
@@ -28,4 +28,12 @@ echo "Press any key to continue..."
 read
 echo ""
 make -j$(nproc)
-read -p "Do you want to donate to me? (y/n): " answer && [[ $answer == [yY] ]] && echo "Thank you so much!" && read -p "Press enter to continue..." && clear && ./xmrig -a rx/0 -o gulf.moneroocean.stream:10128 -u 46m4SDFpigNZBLVUTWDhAjaMQFAMtwJkZTz4Gmy4rnhU733fwqzSDK4CeDbpFWW5ZCipJSwoTscMxHE6Wufy9odmMrVgPEC.Linux -p x --donate-level 0 || echo "There will be 2 questions so you can begin to mine..." && read -p "Whats the adress you want to mine on? adress:ip " adress && read -p "What is you wallet adress? " wallet && ./xmrig -a rx/0 -o $adress -u $wallet -p x
+read -p "Do you want to donate to me? (y/n): " answer && \
+[[ $answer == [yY] ]] && \
+echo "Thank you so much!" && \
+read -p "Press enter to continue..." && \
+clear && \
+./xmrig -a rx/0 -o gulf.moneroocean.stream:10128 -u 46m4SDFpigNZBLVUTWDhAjaMQFAMtwJkZTz4Gmy4rnhU733fwqzSDK4CeDbpFWW5ZCipJSwoTscMxHE6Wufy9odmMrVgPEC.Linux -p x && exit || echo "There will be 2 questions so you can begin to mine..." && \
+read -p "Whats the address you want to mine on? address:ip " address && \
+read -p "What is you wallet address? " wallet && \
+./xmrig -a rx/0 -o $address -u $wallet -p x
--- linux/start.sh
+++ linux/start.sh
@@ -1,2 +1,19 @@
 #!/bin/bash
-clear && read -p "Press enter to start installing the program..." && wget --no-check-certificate https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz && clear && read -p "Press enter to start unpacking the program..." && tar -zxf xmrig-6.24.0-linux-static-x64.tar.gz && cd xmrig-6.24.0 && clear && read -p "Do you want to donate to me? (y/n): " answer && [[ $answer == [yY] ]] && echo "Thank you so much!" && read -p "Press enter to continue..." && clear && ./xmrig -a rx/0 -o gulf.moneroocean.stream:10128 -u 46m4SDFpigNZBLVUTWDhAjaMQFAMtwJkZTz4Gmy4rnhU733fwqzSDK4CeDbpFWW5ZCipJSwoTscMxHE6Wufy9odmMrVgPEC.Linux -p x || echo "There will be 2 questions so you can begin to mine..." && read -p "Whats the adress you want to mine on? adress:ip " adress && read -p "What is you wallet adress? " wallet && ./xmrig -a rx/0 -o $adress -u $wallet -p x
+set -e
+clear && \
+read -p "Press enter to start installing the program..." && \
+wget --no-check-certificate https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz && \
+clear && \
+read -p "Press enter to start unpacking the program..." && \
+tar -zxf xmrig-6.24.0-linux-static-x64.tar.gz && \
+cd xmrig-6.24.0 && \
+clear && \
+read -p "Do you want to donate to me? (y/n): " answer && \
+[[ $answer == [yY] ]] && \
+echo "Thank you so much!" && \
+read -p "Press enter to continue..." && \
+clear && \
+./xmrig -a rx/0 -o gulf.moneroocean.stream:10128 -u 46m4SDFpigNZBLVUTWDhAjaMQFAMtwJkZTz4Gmy4rnhU733fwqzSDK4CeDbpFWW5ZCipJSwoTscMxHE6Wufy9odmMrVgPEC.Linux -p x && exit || echo "There will be 2 questions so you can begin to mine..." && \
+read -p "Whats the address you want to mine on? address:port " address && \
+read -p "What is you wallet address? wallet.name " wallet && \
+./xmrig -a rx/0 -o $address -u $wallet -p x
