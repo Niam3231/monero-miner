0534096b6332ed38a245d449b923853264238152
Niam3231 <niam3231@hotmail.com>
2025-12-18 09:57:37 +0100

    Prettify and add firewall bypass
    By using +128000 after wallet and port 80 you can bypass many firewalls.
    
    Prettify is done by AI because the script looked like a spaghetti noodle and was terrible.
    It's still the same and you won't notice any difference.

--- linux/start.sh
+++ linux/start.sh
@@ -1,19 +1,56 @@
 #!/bin/bash
 set -e
-clear && \
-read -p "Press enter to start installing the program..." && \
-wget --no-check-certificate https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz && \
-clear && \
-read -p "Press enter to start unpacking the program..." && \
-tar -zxf xmrig-6.24.0-linux-static-x64.tar.gz && \
-cd xmrig-6.24.0 && \
-clear && \
-read -p "Do you want to donate to me? (y/n): " answer && \
-[[ $answer == [yY] ]] && \
-echo "Thank you so much!" && \
-read -p "Press enter to continue..." && \
-clear && \
-./xmrig -a rx/0 -o gulf.moneroocean.stream:10128 -u 46m4SDFpigNZBLVUTWDhAjaMQFAMtwJkZTz4Gmy4rnhU733fwqzSDK4CeDbpFWW5ZCipJSwoTscMxHE6Wufy9odmMrVgPEC.Linux -p x && exit || echo "There will be 2 questions so you can begin to mine..." && \
-read -p "Whats the address you want to mine on? address:port " address && \
-read -p "What is you wallet address? wallet.name " wallet && \
-./xmrig -a rx/0 -o $address -u $wallet -p x
+
+clear
+read -p "Press enter to start installing the program..."
+
+wget --no-check-certificate https://github.com/xmrig/xmrig/releases/download/v6.24.0/xmrig-6.24.0-linux-static-x64.tar.gz
+
+clear
+read -p "Press enter to start unpacking the program..."
+
+tar -zxf xmrig-6.24.0-linux-static-x64.tar.gz
+cd xmrig-6.24.0
+
+clear
+read -p "Are you on a restricted network? (y/n): " restricted
+
+# defaults
+PORT=10128
+
+if [[ $restricted == [yY] ]]; then
+    PORT=80
+    RESTRICT_SUFFIX="+128000"
+else
+    RESTRICT_SUFFIX=""
+fi
+
+clear
+read -p "Do you want to donate to me? (y/n): " donate
+
+if [[ $donate == [yY] ]]; then
+    echo "Thank you so much!"
+    read -p "Press enter to continue..."
+
+    DONATE_WALLET="46m4SDFpigNZBLVUTWDhAjaMQFAMtwJkZTz4Gmy4rnhU733fwqzSDK4CeDbpFWW5ZCipJSwoTscMxHE6Wufy9odmMrVgPEC.Linux"
+    DONATE_WALLET="${DONATE_WALLET}${RESTRICT_SUFFIX}"
+
+    ./xmrig -a rx/0 \
+        -o gulf.moneroocean.stream:${PORT} \
+        -u "$DONATE_WALLET" \
+        -p x
+
+    exit
+fi
+
+echo "There will be 2 questions so you can begin to mine..."
+
+read -p "Whats the pool address (host only): " host
+read -p "What is your wallet address? wallet.name " wallet
+
+wallet="${wallet}${RESTRICT_SUFFIX}"
+
+./xmrig -a rx/0 \
+    -o ${host}:${PORT} \
+    -u "$wallet" \
+    -p x
